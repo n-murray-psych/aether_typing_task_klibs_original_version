60d3d03c9053c0eb422939e08483efb551c6ad2e
Nick Murray <nickmurray@Nicks-MacBook-Pro.local>
2026-01-14 16:17:54 -0400

    Comments added to make experiment.py template file more readable

--- aether_typing_study_experiment/experiment.py
+++ aether_typing_study_experiment/experiment.py
@@ -5,18 +5,22 @@ __author__ = "Nicholas Murray"
 import klibs
 from klibs import P
 
-
+# Entire experiment object
 class aether_typing_study_experiment(klibs.Experiment):
 
+    # Define all experimental stimuli drawn on the screen
     def setup(self):
         pass
 
+    # Define block order
     def block(self):
         pass
 
+    # Define what kind of trial this is
     def trial_prep(self):
         pass
 
+    # Define trial order of events
     def trial(self):
 
         return {
