44b2a6e71affe8c2f3339a83f6dd92d1d2ce40f8
Nick Murray <nickmurray@Nicks-MacBook-Pro.local>
2026-01-14 21:18:18 -0400

    Make text appear on the left side, and the query appear on the right side

--- aether_typing_study_experiment/experiment.py
+++ aether_typing_study_experiment/experiment.py
@@ -5,8 +5,8 @@ __author__ = "Nicholas Murray"
 import klibs
 from klibs import P
 from klibs.KLUserInterface import any_key
-from klibs.KLGraphics import fill, flip
-from klibs.KLCommunication import user_queries, query, query_no_backspace
+from klibs.KLGraphics import fill, blit, flip
+from klibs.KLCommunication import user_queries, query, query_no_backspace, message
 
 # Entire experiment object
 class aether_typing_study_experiment(klibs.Experiment):
@@ -22,18 +22,25 @@ class aether_typing_study_experiment(klibs.Experiment):
     # Define what kind of trial this is
     def trial_prep(self):
         pass
-
     # Define trial order of events
     def trial(self):
 
+        left_text = "text for sample"  # whatever your big text is
+
+        def draw_left():
+            # left column: top-left, wrapped
+            msg = message(left_text, align="left", wrap_width=int(P.screen_x * 0.45), blit_txt=False)
+            blit(msg, 1, (int(P.screen_x * 0.05), int(P.screen_y * 0.10)))
+
         typed_text = query_no_backspace(
-            user_queries.experimental[0]
+            user_queries.experimental[0],
+            draw_left_fn=draw_left
         )
 
         return {
             "block_num": P.block_number,
             "trial_num": P.trial_number,
-            "breakfast_question_response": typed_text
+            "typed_response": typed_text
         }
 
     def trial_clean_up(self):
